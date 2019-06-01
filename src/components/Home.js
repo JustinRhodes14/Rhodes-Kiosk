3467b73e9f0269dd6a7ace5d992ad1bba5ac8cb8
JustinRhodes14 <46693438+JustinRhodes14@users.noreply.github.com>
2019-06-01 15:13:27 -0400

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => (
   <div>
     <h1 className = "title-container__title"> National Park Kiosk </h1>
     <h3 className = "title-container__subtitle"> Everything you want to know, consolidated for ease of access! </h3>
-    <h5 className = "title-container__subtitle"> To start searching, check out the list of park codes <a href = "https://nps-vsp.warnercnr.colostate.edu/startpage.php"> <u><b>Click Here!</b></u></a> </h5>
+    <h5 className = "title-container__subtitle"> To start searching, check out the list of park codes <a href = "https://nps-vsp.warnercnr.colostate.edu/startpage.php"> <u><b>here!</b></u></a> </h5>
   </div>
 );
 
