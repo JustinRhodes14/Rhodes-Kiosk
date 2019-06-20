e15884b50940a05c31e498a772fdf173efe17838
JustinRhodes14 <rhodesju21@gmail.com>
2019-06-20 18:37:10 -0400

    Updates

---
+++ precache-manifest.c06095d569f92606f2e9b19abfb3f7ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "557b0c326ffcb929bd11c43b13419c22",
+    "url": "/Rhodes-Kiosk/index.html"
+  },
+  {
+    "revision": "def4dd88c05be1c7cdb5",
+    "url": "/Rhodes-Kiosk/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "95305df1ee81eef38a12",
+    "url": "/Rhodes-Kiosk/static/css/main.32444fd6.chunk.css"
+  },
+  {
+    "revision": "def4dd88c05be1c7cdb5",
+    "url": "/Rhodes-Kiosk/static/js/2.5378fd1e.chunk.js"
+  },
+  {
+    "revision": "95305df1ee81eef38a12",
+    "url": "/Rhodes-Kiosk/static/js/main.b73a9d8c.chunk.js"
+  },
+  {
+    "revision": "88e1f94ebb65a2738878",
+    "url": "/Rhodes-Kiosk/static/js/runtime~main.3e91b3ac.js"
+  },
+  {
+    "revision": "801ea3c2574702f181e0f982459a95a6",
+    "url": "/Rhodes-Kiosk/static/media/bg.801ea3c2.jpg"
+  }
+]);
